8123b904d8d9c2c18a5066e967a97af70407e959
ganiserb <g.gabriel.b@gmail.com>
2014-04-05 02:50:41 -0300

    Added a rollback feature

--- setup-ThunderbirdFilterSync.sh
+++ setup-ThunderbirdFilterSync.sh
@@ -1,18 +1,38 @@
 #!/bin/bash
 
-TB_PROFILE=/home/gabriel/.thunderbird/ThunderbirdProfile
-SYNC_STASH=/home/gabriel/Dropbox/Sistema/ThunderbirdSync
+TB_PROFILE="$1"
+SYNC_STASH="$2"
 
-for accountDir in $(ls -1p $TB_PROFILE$'/ImapMail' | grep "/")
-do  # accountDir has a trailing slash: account1/
-    accountName=${accountDir%/}
-    cd $TB_PROFILE$'/ImapMail' # Move to Profile > ImapMail
-    
-    accountStash=$SYNC_STASH$"/"$accountName
+if "$3" = "restore"; then
+	for accountDir in $(ls -1p $TB_PROFILE$'/ImapMail' | grep "/");	do
+		# accountDir has a trailing slash: account1/
+		accountName=${accountDir%/}
+		
+		cd $TB_PROFILE$'/ImapMail' # Move to Profile > ImapMail
+		
+		accountStash=$SYNC_STASH$"/"$accountName
 
-    mkdir -p $accountStash$"/"
-    cp $accountName$"/msgFilterRules.dat" $accountStash$"/"
-    mv $accountName$"/msgFilterRules.dat" $accountName$"/msgFilterRules.dat.backup"
-    
-    ln -s $accountStash$"/msgFilterRules.dat" $accountName$"/"msgFilterRules.dat
-done
+		rm $accountName$"/msgFilterRules.dat"	# The (supposed) symlink
+		cp $accountName$"/msgFilterRules.dat.backup" $accountName$"/msgFilterRules.dat"
+	done
+
+elif "$3" = "update"; then
+
+	exit
+
+else
+	for accountDir in $(ls -1p $TB_PROFILE$'/ImapMail' | grep "/");	do
+		# accountDir has a trailing slash: account1/
+		accountName=${accountDir%/}
+		
+		cd $TB_PROFILE$'/ImapMail' # Move to Profile > ImapMail
+		
+		accountStash=$SYNC_STASH$"/"$accountName
+
+		mkdir -p $accountStash$"/"
+		cp $accountName$"/msgFilterRules.dat" $accountStash$"/"
+		mv $accountName$"/msgFilterRules.dat" $accountName$"/msgFilterRules.dat.backup"
+		
+		ln -s $accountStash$"/msgFilterRules.dat" $accountName$"/"msgFilterRules.dat
+	done
+fi
