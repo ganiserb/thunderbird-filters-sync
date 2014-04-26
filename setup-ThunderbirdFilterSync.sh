c1fff0fa34fa81dabf154b73d31c9c2adfa7bb45
ganiserb <g.gabriel.b@gmail.com>
2014-04-26 18:47:44 -0300

    Fixed and complete

--- setup-ThunderbirdFilterSync.sh
+++ setup-ThunderbirdFilterSync.sh
@@ -1,38 +1,66 @@
 #!/bin/bash
 
-TB_PROFILE="$1"
-SYNC_STASH="$2"
+# Usage:
+# 	script.sh <absolute_thunderbird_profile_path> <absolute_sync_dir_path> [restore|link]
+#	
+#	If the script is called without a third parameter, it will asume this is
+#		the first time the links are being set up and will replace all existing
+#		files inside the sync_dir
+#	If called wit the "restore" parameter, the script will restore the backup files inside
+#		the thunderbird_profile created when the script was first run, and leave the files
+#		inside the sync_dir untouched
+#	"link" currently does nothing but should link to an existing stash inside the sync_dir
+#
+
+# Both parameters WITHOUT trailing slash
+TB_PROFILE="$1" # Full path to your Thunderbird profile folder
+SYNC_STASH="$2" # Full path to your Synced folder (ie: Dropbox) where to put the filter rules
 
 if [ "$3" = "restore" ]; then
-	for accountDir in $(ls -1p $TB_PROFILE$'/ImapMail' | grep "/");	do
+	# 
+	
+	for accountDir in $(ls -1p $TB_PROFILE/ImapMail | grep "/");	do
 		# accountDir has a trailing slash: account1/
 		accountName=${accountDir%/}
 		
-		cd $TB_PROFILE$'/ImapMail' # Move to Profile > ImapMail
+		cd $TB_PROFILE/ImapMail # Move to Profile > ImapMail
 		
-		accountStash=$SYNC_STASH$"/"$accountName
+		accountStash=$SYNC_STASH"/"$accountName
 
-		rm $accountName$"/msgFilterRules.dat"	# The (supposed) symlink
-		cp $accountName$"/msgFilterRules.dat.backup" $accountName$"/msgFilterRules.dat"
+		rm $accountName"/msgFilterRules.dat"	# The (supposed) symlink
+		cp $accountName"/msgFilterRules.dat.backup" $accountName"/msgFilterRules.dat"
 	done
 
-elif [ "$3" = "update" ]; then
+elif [ "$3" = "link" ]; then
 
-	exit
-
-else
-	for accountDir in $(ls -1p $TB_PROFILE$'/ImapMail' | grep "/");	do
+	for accountDir in $(ls -1p $TB_PROFILE/ImapMail | grep "/");	do
 		# accountDir has a trailing slash: account1/
 		accountName=${accountDir%/}
 		
-		cd $TB_PROFILE$'/ImapMail' # Move to Profile > ImapMail
+		cd $TB_PROFILE'/ImapMail' # Move to Profile > ImapMail
 		
-		accountStash=$SYNC_STASH$"/"$accountName
+		accountStash=$SYNC_STASH"/"$accountName
 
-		mkdir -p $accountStash$"/"
-		cp $accountName$"/msgFilterRules.dat" $accountStash$"/"
-		mv $accountName$"/msgFilterRules.dat" $accountName$"/msgFilterRules.dat.backup"
+		#mkdir -p $accountStash$"/"
+		#cp $accountName$"/msgFilterRules.dat" $accountStash$"/"
+		mv $accountName"/msgFilterRules.dat" $accountName"/msgFilterRules.dat.backup"
 		
-		ln -s $accountStash$"/msgFilterRules.dat" $accountName$"/"msgFilterRules.dat
+		ln -s $accountStash"/msgFilterRules.dat" $accountName"/msgFilterRules.dat"
+	done
+
+else
+	for accountDir in $(ls -1p $TB_PROFILE/ImapMail | grep "/");	do
+		# accountDir has a trailing slash: account1/
+		accountName=${accountDir%/}
+		
+		cd $TB_PROFILE'/ImapMail' # Move to Profile > ImapMail
+		
+		accountStash=$SYNC_STASH"/"$accountName
+
+		mkdir -p $accountStash"/"
+		cp $accountName"/msgFilterRules.dat" $accountStash"/"
+		mv $accountName"/msgFilterRules.dat" $accountName"/msgFilterRules.dat.backup"
+		
+		ln -s $accountStash"/msgFilterRules.dat" $accountName"/msgFilterRules.dat"
 	done
 fi
